26b512f0c5865efc08f6c3fee4b110b0ce2decd6
joohyun park <joohyun.park@havas.com>
2020-11-10 17:49:33 -0500

    test list

---
+++ components/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ item.text }} {{ new Date() }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      default: () => {},
+      type: Object,
+    },
+  },
+};
+</script>
---
+++ pages/list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div v-for="(item, i) in items" :key="i">
+      {{ item.text }} {{ new Date() }}
+    </div>
+
+    <button @click="click">click</button>
+    <button @click="click2">click2</button>
+
+    <List :item="item" v-for="(item, i) in items" :key="i" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 0, text: "zero" },
+        { id: 1, text: "one" },
+        { id: 2, text: "two" },
+      ],
+    };
+  },
+  methods: {
+    click() {
+      this.items = [
+        { id: 0, text: "zero" },
+        { id: 1, text: "one---" },
+        { id: 2, text: "two" },
+      ];
+    },
+    click2() {
+      this.$set(this.items, 1, { id: 1, text: "one---" });
+    },
+  },
+};
+</script>

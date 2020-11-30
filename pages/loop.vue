d8495d34ccc25557795a31d52ed206cdc3e56e85
joohyun park <joohyun.park@havas.com>
2020-11-30 13:30:40 -0500

    test duffs' device

--- pages/loop.vue
+++ pages/loop.vue
@@ -13,12 +13,17 @@ export default {
         { id: 3, text: "three" },
         { id: 4, text: "four" },
         { id: 5, text: "five" },
+        { id: 6, text: "six" },
+        { id: 7, text: "seven" },
+        { id: 8, text: "eight" },
+        { id: 9, text: "nine" },
+        { id: 10, text: "ten" },
       ],
     };
   },
   mounted() {
     // minimizing property lookups
-    // around 25% faster
+    // up to 25% faster
     for (var i = 0, len = this.items.length; i < len; i++) {
       console.log(i);
     }
@@ -26,10 +31,50 @@ export default {
     console.log("-----");
 
     // minimizing property lookups and reversing
-    // around 50–60% faster
+    // up to 50–60% faster
     for (var i = this.items.length; i--; ) {
       console.log(i);
     }
+
+    console.log("-----");
+
+    //credit: Jeff Greenberg
+    var iterations = Math.floor(this.items.length / 8),
+      startAt = this.items.length % 8,
+      i = 0;
+
+    do {
+      console.log("iteration: ", iterations);
+      switch (startAt) {
+        case 0:
+          console.log(i);
+          i++;
+        case 7:
+          console.log(i);
+          i++;
+        case 6:
+          console.log(i);
+          i++;
+        case 5:
+          console.log(i);
+          i++;
+        case 4:
+          console.log(i);
+          i++;
+        case 3:
+          console.log(i);
+          i++;
+        case 2:
+          console.log(i);
+          i++;
+        case 1:
+          console.log(i);
+          i++;
+      }
+      startAt = 0;
+      console.log("iteration: ", iterations);
+    } while (iterations--);
   },
+  methods: {},
 };
 </script>
